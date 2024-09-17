6120cc778343c95485f09e274b20d7a7c6293627
tutich <pkoinange02@gmail.com>
2024-09-17 18:41:54 +0300

    complete project

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -2,7 +2,6 @@ import { Box, Text, Divider, IconButton, Image, Button, Flex, useColorMode } fro
 import { useState } from 'react';
 import iconCheck from '../images/icon-check.svg'; // Path to your check icon image
 import iconCross from '../images/icon-cross.svg'; // Path to your cross icon image
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const TodoCard = ({ todos, setTodos }) => {
   const { colorMode } = useColorMode(); // Get the current color mode (light or dark)
@@ -38,162 +37,131 @@ const TodoCard = ({ todos, setTodos }) => {
     return true;
   });
 
-  // Handle drag end event
-  const onDragEnd = (result) => {
-    const { source, destination } = result;
-
-    if (!destination) return;
-
-    const updatedTodos = Array.from(todos);
-    const [movedTodo] = updatedTodos.splice(source.index, 1);
-    updatedTodos.splice(destination.index, 0, movedTodo);
-
-    setTodos(updatedTodos);
-  };
-
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="todos">
-        {(provided) => (
+    <Box
+      p={4}
+      w="100%"
+      bg={colorMode === "light" ? "white" : "#1a202c"}  // Set background color based on color mode
+      borderRadius="md"
+      boxShadow="sm"
+      border="1px solid"
+      borderColor={colorMode === "light" ? "gray.200" : "gray.700"} // Adjust border color for dark mode
+    >
+      {filteredTodos.map((todo, index) => (
+        <Box key={index}>
           <Box
-            p={4}
-            w="100%"
-            bg={colorMode === "light" ? "white" : "#1a202c"}  // Set background color based on color mode
-            borderRadius="md"
-            boxShadow="sm"
-            border="1px solid"
-            borderColor={colorMode === "light" ? "gray.200" : "gray.700"} // Adjust border color for dark mode
-            ref={provided.innerRef}
-            {...provided.droppableProps}
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
           >
-            {filteredTodos.map((todo, index) => (
-              <Draggable key={index} draggableId={`todo-${index}`} index={index}>
-                {(provided) => (
-                  <Box
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    mb={2}
-                  >
+            <Box display="flex" alignItems="center">
+              {/* Checkbox or Check Icon */}
+              <IconButton
+                onClick={() => toggleComplete(index)}
+                aria-label="Complete Todo"
+                icon={
+                  completed.includes(index) ? (
                     <Box
+                      w="1.2rem"
+                      h="1.2rem"
                       display="flex"
-                      justifyContent="space-between"
+                      justifyContent="center"
                       alignItems="center"
-                      onMouseEnter={() => setHoveredIndex(index)}
-                      onMouseLeave={() => setHoveredIndex(null)}
+                      borderRadius="full"
+                      bg="linear-gradient(to bottom right, hsl(192, 100%, 67%), hsl(280, 87%, 65%))"
+                      p="0.3rem"
                     >
-                      <Box display="flex" alignItems="center">
-                        {/* Checkbox or Check Icon */}
-                        <IconButton
-                          onClick={() => toggleComplete(index)}
-                          aria-label="Complete Todo"
-                          icon={
-                            completed.includes(index) ? (
-                              <Box
-                                w="1.2rem"
-                                h="1.2rem"
-                                display="flex"
-                                justifyContent="center"
-                                alignItems="center"
-                                borderRadius="full"
-                                bg="linear-gradient(to bottom right, hsl(192, 100%, 67%), hsl(280, 87%, 65%))"
-                                p="0.3rem"
-                              >
-                                <Image src={iconCheck} alt="check" />
-                              </Box>
-                            ) : (
-                              <Box
-                                w="1.2rem"
-                                h="1.2rem"
-                                border="2px solid gray"
-                                borderRadius="full"
-                              />
-                            )
-                          }
-                          variant="ghost"
-                          size="sm"
-                          mr={3}
-                        />
-
-                        {/* Todo Text with strikethrough if completed */}
-                        <Text
-                          as={completed.includes(index) ? "del" : "span"}
-                          color={
-                            colorMode === "light"
-                              ? completed.includes(index)
-                                ? "gray.500" // Light mode completed
-                                : "black"    // Light mode active
-                              : completed.includes(index)
-                                ? "gray.500" // Dark mode completed
-                                : "gray.300" // Dark mode active
-                          }
-                        >
-                          {todo}
-                        </Text>
-                      </Box>
-
-                      {/* Display the cross icon when the todo is hovered */}
-                      {hoveredIndex === index && (
-                        <IconButton
-                          onClick={() => handleDeleteTodo(index)}
-                          aria-label="Delete Todo"
-                          icon={<Image src={iconCross} alt="delete" />}
-                          variant="ghost"
-                          size="sm"
-                        />
-                      )}
+                      <Image src={iconCheck} alt="check" />
                     </Box>
+                  ) : (
+                    <Box
+                      w="1.2rem"
+                      h="1.2rem"
+                      border="2px solid gray"
+                      borderRadius="full"
+                    />
+                  )
+                }
+                variant="ghost"
+                size="sm"
+                mr={3}
+              />
 
-                    {/* Divider below the todo item */}
-                    <Divider mt={2} mb={2} />
-                  </Box>
-                )}
-              </Draggable>
-            ))}
+              {/* Todo Text with strikethrough if completed */}
+              <Text
+                as={completed.includes(index) ? "del" : "span"}
+                color={
+                    colorMode === "light"
+                      ? completed.includes(index)
+                        ? "gray.500" // Light mode completed
+                        : "black"    // Light mode active
+                      : completed.includes(index)
+                        ? "gray.500" // Dark mode completed
+                        : "gray.300" // Dark mode active
+                  }
+              >
+                {todo}
+              </Text>
+            </Box>
 
-            {/* Footer with Todo summary and filter options */}
-            <Flex justify="space-between" align="center" mt={4}>
-              {/* Items left */}
-              <Text>{todos.length - completed.length} items left</Text>
-
-              {/* Filter Buttons */}
-              <Flex justify="center" align="center">
-                <Button
-                  variant="link"
-                  mx={2}
-                  onClick={() => setFilter('all')}
-                  isActive={filter === 'all'}
-                >
-                  All
-                </Button>
-                <Button
-                  variant="link"
-                  mx={2}
-                  onClick={() => setFilter('active')}
-                  isActive={filter === 'active'}
-                >
-                  Active
-                </Button>
-                <Button
-                  variant="link"
-                  mx={2}
-                  onClick={() => setFilter('completed')}
-                  isActive={filter === 'completed'}
-                >
-                  Completed
-                </Button>
-              </Flex>
-
-              {/* Clear Completed Button */}
-              <Button variant="link" onClick={handleClearCompleted}>
-                Clear Completed
-              </Button>
-            </Flex>
-            {provided.placeholder}
+            {/* Display the cross icon when the todo is hovered */}
+            {hoveredIndex === index && (
+              <IconButton
+                onClick={() => handleDeleteTodo(index)}
+                aria-label="Delete Todo"
+                icon={<Image src={iconCross} alt="delete" />}
+                variant="ghost"
+                size="sm"
+              />
+            )}
           </Box>
-        )}
-      </Droppable>
-    </DragDropContext>
+
+          {/* Divider below the todo item */}
+          <Divider mt={2} mb={2} />
+        </Box>
+      ))}
+
+      {/* Footer with Todo summary and filter options */}
+      <Flex justify="space-between" align="center" mt={4}>
+        {/* Items left */}
+        <Text>{todos.length - completed.length} items left</Text>
+
+        {/* Filter Buttons */}
+        <Flex justify="center" align="center">
+          <Button
+            variant="link"
+            mx={2}
+            onClick={() => setFilter('all')}
+            isActive={filter === 'all'}
+          >
+            All
+          </Button>
+          <Button
+            variant="link"
+            mx={2}
+            onClick={() => setFilter('active')}
+            isActive={filter === 'active'}
+          >
+            Active
+          </Button>
+          <Button
+            variant="link"
+            mx={2}
+            onClick={() => setFilter('completed')}
+            isActive={filter === 'completed'}
+          >
+            Completed
+          </Button>
+        </Flex>
+
+        {/* Clear Completed Button */}
+        <Button variant="link" onClick={handleClearCompleted}>
+          Clear Completed
+        </Button>
+      </Flex>
+    </Box>
   );
 };
 
